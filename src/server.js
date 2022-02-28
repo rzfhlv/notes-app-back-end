7991a9685fd37e9bd01d251114810ac4b48bf610
rzfhlv <rezafahlevi.ilkom49@gmail.com>
2022-02-28 17:56:13 +0700

    mantap

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
+  const notesService = new NotesService();
+
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
@@ -12,7 +18,13 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
